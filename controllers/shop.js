597872e12e99fc77ffdd714fba23eaf237270b52
DhruvUpadhyaya <2793dhruv@gmail.com>
2020-06-12 15:35:45 +0530

    product model featchAll method updated.

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,14 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res) => {
-	Product.fetchAll((products) => {
-		res.render('shop/product-list', {
-			prods: products,
-			pageTitle: 'Products',
-			path: '/products',
-			hasProducts: products.length > 0,
+	Product.fetchAll()
+		.then(([row, fieldData]) => {
+			res.render('shop/product-list', {
+				prods: row,
+				pageTitle: 'Products',
+				path: '/products',
+			});
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	});
 };
 
 exports.getProduct = (req, res) => {
@@ -24,14 +27,17 @@ exports.getProduct = (req, res) => {
 };
 
 exports.getIndex = (req, res) => {
-	Product.fetchAll((products) => {
-		res.render('shop/index', {
-			prods: products,
-			pageTitle: 'All Products',
-			path: '/',
-			hasProducts: products.length > 0,
+	Product.fetchAll()
+		.then(([row, fieldData]) => {
+			res.render('shop/index', {
+				prods: row,
+				pageTitle: 'All Products',
+				path: '/',
+			});
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	});
 };
 
 exports.getCart = (req, res) => {
